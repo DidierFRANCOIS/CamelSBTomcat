14062a3ec6439c3f6edaf47df30ea9ee78fc045f
DidierFRANCOIS <didier.francois44@gmail.com>
2018-04-02 20:44:32 +0200

    First commit

---
+++ src/main/java/sprigboot/basic/CamelSbTomcatApplication.java
@@ -0,0 +1,12 @@
+package sprigboot.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSbTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSbTomcatApplication.class, args);
+	}
+}
---
+++ src/main/java/sprigboot/basic/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sprigboot.basic;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CamelSbTomcatApplication.class);
+	}
+
+}
